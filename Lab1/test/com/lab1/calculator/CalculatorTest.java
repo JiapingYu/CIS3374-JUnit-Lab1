9a4b2f7fbec6b10b9983c8035c329314162f980b
JiapingYu <tug77110@temple.edu>
2017-03-03 15:20:28 -0500

    Limitations
    This kind of test could only cover limited situations, for example:
    calculator.multiply(2,2) could also pass the add test. because 2+2=2*2.

---
+++ Lab1/test/com/lab1/calculator/CalculatorTest.java
@@ -0,0 +1,37 @@
+package com.lab1.calculator;
+
+import static org.junit.Assert.*;
+
+
+import org.junit.Test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+	private Calculator calculator;
+	
+	@Before
+	public void setUp() {
+		calculator = new Calculator();
+	}
+	
+	@Test
+	public void addtest() {
+		assertEquals(4, calculator.add(2, 2));
+	}
+	public void subtracttest(){
+		assertEquals(0, calculator.subtract(2, 2));
+	}
+	public void Multiplytest() {
+		assertEquals(6, calculator.multiply(2, 3));
+	}
+	public void dividetest() {
+		assertEquals(1, calculator.divide(2, 2));
+	}
+	public void equaltest(){
+		assertTrue(calculator.isEqual(2, 2));
+		assertFalse(calculator.isEqual(2, 3));
+	}
+
+}
